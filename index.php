36f66e121683d237726cd075811f08c06283f5b2
Elodby <elodie.bironneau@hotmail.fr>
2015-05-13 23:23:36 +0200

    indes

--- index.php
+++ index.php
@@ -96,8 +96,7 @@
     $app->response()->header('Content-Type: application/json; Charset="UTF-8"');
     echo json_encode($copy, JSON_UNESCAPED_UNICODE);
   }); 
-
-  });  
+ 
   $app->post('/copy/:id', function($id) use($app){
     $result = Copy::delete_copy($id);
     $app->response()->header('Content-Type: application/json; Charset="UTF-8"');
